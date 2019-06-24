803fc48f74f2fbc54c894eca8c9244ec9b52078f
Wan-Yifei <yifei.wan@admerahealth.com>
2019-06-24 18:34:31 -0400

    Feat: Increase the cutoff of length of read to 150 File: Feature_Counts.sh

--- ATACseq/pipeline/Feature_Counts.sh
+++ ATACseq/pipeline/Feature_Counts.sh
@@ -11,5 +11,5 @@ do
     echo input: $filename
     echo output: $saf_and_output/${outname}.count.txt
     echo
-    /home/yifei.wan/Tools/subread-1.6.3-source/bin/featureCounts -F SAF -T 64 -P -p -d 0 -D 100 -a $saf_and_output/*.saf -o $saf_and_output/${filename}.count.txt $f
+    /home/yifei.wan/Tools/subread-1.6.3-source/bin/featureCounts -F SAF -T 64 -P -p -d 0 -D 150 -a $saf_and_output/*.saf -o $saf_and_output/${filename}.count.txt $f
 done
