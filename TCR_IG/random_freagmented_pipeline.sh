11253b7c95d78033a3e998c8f6fce0aba22dadea
Wan-Yifei <yifei.wan@admerahealth.com>
2019-07-18 19:42:32 -0400

    Feat: TCR/IG pipeline File: random_freagmented_pipeline.sh

---
+++ TCR_IG/random_freagmented_pipeline.sh
@@ -0,0 +1,40 @@
+# author: yifei.wan
+# Apply mixcr to analyze TCR/IG data based on random fragmented data (like RNA-Seq, Exome-Seq, etc).
+
+set -e
+
+input=$1 ## folder of input fastq file
+output=$2 ## result
+
+check_folder(){
+    if [[ -d ${output}/${prefix} ]]
+    then
+        echo ------------------------------------
+        echo Output folder: $prefix present!
+        echo ------------------------------------
+    else
+        echo ------------------------------------
+        echo Make folder: $prefix
+        mkdir ${output}/${prefix}
+        echo ------------------------------------
+    fi
+}
+
+for fq in ${input}/*R1*.fastq.gz
+do
+    file=$(basename $fq)
+    name=${file%%_R1_001.fastq.gz}
+    prefix=${name%%_L001}
+    fq2=${input}/${name}_R2_001.fastq.gz
+    echo ====================================
+    echo Input FASTQ files:
+    echo $fq
+    echo $fq2
+    check_folder
+    cd ${output}/${prefix}
+    /home/yifei.wan/.linuxbrew/bin/mixcr analyze shotgun \
+            --species hsa \
+            --starting-material dna \
+            $fq $fq2 $prefix
+done
+
