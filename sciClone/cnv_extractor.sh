fe362716de8a6d830a066bc528e6f474610a5df4
Wan-Yifei <yifei.wan@admerahealth.com>
2019-07-25 16:12:57 -0400

    Feat: Extract CNV from TSV files File: cnv_extractor.sh

---
+++ sciClone/cnv_extractor.sh
@@ -0,0 +1,18 @@
+# author: Yifei.wan
+
+# Extract CNV from TSV file
+
+input=$1 ## input folder
+output=$2 ## output folder
+
+echo ===============================
+echo Format: chr   start        stop    segment_mean 
+for p in $input/*.tsv
+do
+    file=$(basename $p)
+    name=${file%%.vcf}
+    echo -------------------------------
+    echo Extract content from $name!
+    awk -F"\t" 'BEGIN{OFS="\t"} /chrom/{next}{print $1, $2, $3, $4}' $p | sed 's/%//g' | sed 's/chr//g' > ${output}/${name}_cnv.txt 
+done
+
