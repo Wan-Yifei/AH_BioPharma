02de39bd481ec0571133bbcdc58122e5cd56f992
Wan-Yifei <yifei.wan@admerahealth.com>
2019-07-25 19:00:10 -0400

    Fix: Input file should be TSV File: cnv_extractor.sh

--- sciClone/cnv_extractor.sh
+++ sciClone/cnv_extractor.sh
@@ -10,7 +10,7 @@ echo Format: chr   start        stop    segment_mean
 for p in $input/*.tsv
 do
     file=$(basename $p)
-    name=${file%%.vcf}
+    name=${file%%.tsv}
     echo -------------------------------
     echo Extract content from $name!
     awk -F"\t" 'BEGIN{OFS="\t"} /chrom/{next}{print $1, $2, $3, $4}' $p | sed 's/%//g' | sed 's/chr//g' > ${output}/${name}_cnv.txt 
