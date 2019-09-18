a07de7c92eecb75759adb34ad984fe1f1e5b1217
Wan-Yifei <yifei.wan@admerahealth.com>
2019-09-18 21:03:44 +0000

    Feat: esATAC pipeline File: esatac_basic_pipeline.R

--- ATACseq/pipeline/esatac_basic_pipeline.R
+++ ATACseq/pipeline/esatac_basic_pipeline.R
@@ -4,9 +4,10 @@ suppressMessages(library("optparse"))
 
 optionList <- list(make_option(c("-i", "--input"), type="character", default=NULL, 
     help="folder of input Fastq files."),
-    make_option(c("-m","--meta"),type="character",default=NULL,help="metadata file [the format of the file is shown in \"meta_demo.txt\"]"),
-    make_option(c("-o","--output"),type="character",default=NULL,help="output folder"),
-    make_option(c("-r","--ref"),type="character",default=NULL,help="type of reference genome [e.g. hg19 or mm10]"))
+    make_option(c("-m", "--meta"), type="character", default=NULL, help="metadata file [the format of the file is shown in \"meta_demo.txt\"]"),
+    make_option(c("-o", "--output"), type="character", default=NULL, help="output folder"),
+    make_option(c("-r", "--ref"), type="character", default=NULL, help="type of reference genome [e.g. hg19 or mm10]"),
+    make_option(c("-t", "--threads"), type="integer", default=NULL, help="the number of threads"))
 
 opt_parser <- OptionParser(option_list=optionList)
 opt <- parse_args(opt_parser)
@@ -15,7 +16,10 @@ if (is.null(opt$input)||is.null(opt$output)||is.null(opt$meta)||is.null(opt$ref)
   print_help(opt_parser)
   }
 
-input = opt$input
+input = opt$input ## input folder
+thread = opt$threads
+ref_type = opt$ref
+ref_genome <- "/data/TG/RUO_Yifei/esATAC_pipeline/refdir"
 setwd(input)
 
 # Prepare the FASTQ for each sample
@@ -49,18 +53,24 @@ sapply(groups, make_folder, output) ## create folder for each group
 
 # Run esATAC pipeline for each sample independently 
 cat("\n")
-#suppressMessages(library(esATAC))
+suppressMessages(library(esATAC))
 for (i in 1:length(samples)){ ## column of FASTQ dataframe has sample order with samples
     group = meta_table$condition[i]
     group_folder = paste(output, group, sep="/")
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-    print(">>>>>>>>>>>   Create folders for each group   >>>>>>>>>")
+    print(">>>>>>>>>>>        Process each sample        >>>>>>>>>")
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     sample_folder = make_folder(samples[i], group_folder) ## create the sub-folder for each sample
     inter_folder = make_folder("internediate_result", sample_folder)
     setwd(sample_folder)
     atacPipe(
-        fastqInput1 = file.path(input, 
+        fastqInput1 = file.path(input, samples_fastq[1, which(colnames(samples_fastq) == make.names(samples[i]))]), 
+        fastqInput2 = file.path(input, samples_fastq[2, which(colnames(samples_fastq) == make.names(samples[i]))]), 
+        refdir = ref_genome, 
+        tmpdir = sample_folder,
+        threads = thread,
+        genome = ref_type 
+        )
     }
 
     
